238270cf973e8a821c53fb4cb448b564868bf61f
eunbin <kozilee00@gmail.com>
2025-06-27 17:53:01 +0900

    jsp/servlet _ lec06 : EL과 JSTL

---
+++ lec06_jstl/src/main/java/com/gn/dto/Person.java
@@ -0,0 +1,14 @@
+package com.gn.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Person {
+	private String name;
+	private int age;
+	
+
+}
---
+++ lec06_jstl/src/main/java/com/gn/dto/Planet.java
@@ -0,0 +1,14 @@
+package com.gn.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Planet {
+	private String name;
+	private double distance;
+	private boolean b;
+
+}
